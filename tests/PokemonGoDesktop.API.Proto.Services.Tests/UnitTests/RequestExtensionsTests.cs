45b8d11b987e3ecb2be01f010b7f4ca8a21107b8
Andrew Blakely <andrew.blakely@ymail.com>
2016-07-21 21:22:42 -0500

    Added Unit Tests for Request Ext

---
+++ tests/PokemonGoDesktop.API.Proto.Services.Tests/UnitTests/RequestExtensionsTests.cs
@@ -0,0 +1,36 @@
+﻿using Networking.Requests;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PokemonGoDesktop.API.Proto.Services.Tests
+{
+	[TestFixture]
+	public class RequestExtensionsTests
+	{
+		[Test]
+		public static void Test_Request_Packs_In_Non_Null_Envelope()
+		{
+			//arrange
+			Request r = new Request();
+
+			//assert
+			Assert.NotNull(r.PackInEnvelope());
+		}
+
+		[Test]
+		public static void Test_Request_Packs_Throws_Null_If_Request_Is_Null()
+		{
+			//arrange
+			Request r = null;
+
+			//assert
+			ArgumentNullException ex = Assert.Throws<ArgumentNullException>(() => r.PackInEnvelope());
+
+			//Check the exception contains the right parameter
+			Assert.AreEqual("request", ex.ParamName);
+		}
+	}
+}
---
+++ tests/RunTests.sh
@@ -0,0 +1,1 @@
+mono ./testrunner/NUnit.Console.3.0.1/tools/nunit3-console.exe ./PokemonGoDesktop.API.Proto.Services.Tests/bin/Debug/PokemonGoDesktop.API.Proto.Services.Tests.dll
