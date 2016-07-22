dc7b5d0f073b54817ca1452a6c800b364440dd9c
Andrew Blakely <andrew.blakely@ymail.com>
2016-07-21 20:57:29 -0500

    Added Sesions and AuthTokens

---
+++ src/PokemonGoDesktop.API.Client.Services/Session/IAuthableSession.cs
@@ -0,0 +1,19 @@
+﻿using PokemonGoDesktop.API.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PokemonGoDesktop.API.Client.Services
+{
+	/// <summary>
+	/// Authenticatable session contract.
+	/// </summary>
+	public interface IAuthableSession : ISession
+	{
+		/// <summary>
+		/// Represents the current <see cref="AuthToken"/>.
+		/// </summary>
+		AuthToken Token { get; }
+	}
+}
---
+++ src/PokemonGoDesktop.API.Client.Services/Session/SessionState.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PokemonGoDesktop.API.Client.Services
+{
+	/// <summary>
+	/// Indicates the current session state.
+	/// </summary>
+	[Flags]
+	public enum SessionState
+	{
+		/// <summary>
+		/// Nothing.
+		/// </summary>
+		Default = 0,
+		
+		/// <summary>
+		/// Indicates if the session has been authenticated.
+		/// </summary>
+		Authenticated = 1 << 1,
+
+		/// <summary>
+		/// Indicates if we have a valid AuthTicket.
+		/// </summary>
+		HasValidAuthTicket = 1 << 2
+	}
+}
