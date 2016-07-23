57b62c753bfb0fe3991769ee4f3421998fade0ae
Andrew Blakely <andrew.blakely@ymail.com>
2016-07-23 16:54:12 -0500

    Added AuthenticationDetails POCO

---
+++ src/PokemonGoDesktop.API.Client.Services/Authentication/AuthenticationDetails.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PokemonGoDesktop.API.Client.Services
+{
+	/// <summary>
+	/// Simple POCO that contains the user provided login details.
+	/// </summary>
+	public class AuthenticationDetails
+	{
+		/// <summary>
+		/// String to auth as.
+		/// </summary>
+		public string LoginString { get; }
+
+		/// <summary>
+		/// Authentication password.
+		/// </summary>
+		public string Password { get; }
+
+		public AuthenticationDetails(string loginString, string password)
+		{
+			LoginString = loginString;
+			Password = password;
+		}
+	}
+}
